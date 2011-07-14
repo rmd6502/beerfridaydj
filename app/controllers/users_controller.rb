b0b80241b0eb3201097a81083d6d552f47db1e20
Robert Diamond <rmd6502@gmail.com>
2011-07-14 11:07:28 -0400

    start adding json support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    respond_to do |fmt|
+      fmt.html
+      fmt.json { render :json => @users.to_json }
+    end
   end
 
   def show
     youtube_client = YouTubeIt::Client.new
     @user = User.find(params[:id])
+    respond_to do |fmt|
+      fmt.html
+      fmt.json { render :json => @user.videos.to_json }
+    end
   end
 end
